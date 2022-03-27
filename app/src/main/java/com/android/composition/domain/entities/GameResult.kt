170060ac6d37a53422ebd16198dc812e6ca67a7c
Agatha <agatha.kazak@gmail.com>
2022-03-27 20:43:58 +0300

    dataBinding(with BindingAdapter)

--- app/src/main/java/com/android/composition/domain/entities/GameResult.kt
+++ app/src/main/java/com/android/composition/domain/entities/GameResult.kt
@@ -5,9 +5,9 @@ import kotlinx.parcelize.Parcelize
 
 
 @Parcelize
-data class GameResult (
+data class GameResult(
     val winner: Boolean,
     val countOfRightAnswers: Int,
     val countOfQuestions: Int,
     val gameSettings: GameSettings
-): Parcelable
+) : Parcelable
---
+++ app/src/main/java/com/android/composition/presentation/BindingAdapters.kt
@@ -0,0 +1,60 @@
+package com.android.composition.presentation
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.android.composition.R
+import com.android.composition.domain.entities.GameResult
+
+@BindingAdapter("requiredAnswers")
+fun bindingRequiredAnswers(textView: TextView, count: Int) {
+    textView.text = String.format(
+        textView.context.getString(R.string.required_score),
+        count
+    )
+}
+
+@BindingAdapter("requiredPercents")
+fun bindingRequiredPercents(textView: TextView, count: Int) {
+    textView.text = String.format(
+        textView.context.getString(R.string.required_percentage),
+        count
+    )
+}
+
+@BindingAdapter("scoreAnswers")
+fun bindingScoreAnswers(textView: TextView, count: Int) {
+    textView.text = String.format(
+        textView.context.getString(R.string.score_answers),
+        count
+    )
+}
+
+@BindingAdapter("scorePercentage")
+fun bindingScorePercentage(textView: TextView, gameResult: GameResult) {
+    textView.text = String.format(
+        textView.context.getString(R.string.score_percentage),
+        getPercentOfRightAnswers(gameResult)
+    )
+}
+
+private fun getPercentOfRightAnswers(gameResult: GameResult) = with(gameResult) {
+    if (countOfQuestions == 0) {
+        0
+    } else {
+        ((countOfRightAnswers / countOfQuestions.toDouble()) * 100).toInt()
+    }
+}
+
+@BindingAdapter("resultImage")
+fun bindingImage(imageView: ImageView, winner: Boolean) {
+    imageView.setImageResource(getSmileResId(winner))
+}
+
+private fun getSmileResId(winner: Boolean): Int {
+    return if (winner) {
+        R.drawable.ic_smile
+    } else {
+        R.drawable.ic_sad
+    }
+}
